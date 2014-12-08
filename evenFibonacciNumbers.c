1ed10570126fc5c95db98597193ee70c929b2b9a
Hurkpaburk <Hurkpaburk@users.noreply.github.com>
2014-12-08 12:24:32 +0100

    Create evenFibonacciNumbers.c
    Solved problem 2, Even Fibonacci numbers

---
+++ evenFibonacciNumbers.c
@@ -0,0 +1,16 @@
+int evenFibonacciNumbers(int num) {
+    int sum = 0;
+    int prevNum = 1;
+    int currNum = 2;
+    int tmp;
+    while( prevNum < num ) {
+         if ((currNum % 2) == 0 ) {
+            sum = sum + currNum;
+        }
+        tmp = prevNum;
+        prevNum = currNum;
+        currNum = currNum + tmp;
+       
+    }
+    return sum;
+}
