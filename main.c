deaaeb81ca7220742459b37e9fbc643a8e56e182
Hurkpaburk <Hurkpaburk@users.noreply.github.com>
2014-12-09 06:56:03 +0100

    Update main.c
    Added all function calls to main.c

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 int main() {
     
-    int sum = multiplesOfThreeAndFive(1000);
-    printf("%d\n",sum );
+    int result;
+    
+    result = multiplesOfThreeAndFive(1000);
+    printf("Result of problem 1: %d\n",result);
+    
+    result = evenFibonacciNumbers(4000000);
+    printf("Result of problem 2: %d\n",result);
+    
+    result = largestPrimeFactor(600851475143);
+    printf("Result of problem 3: %d\n",result);
     
 }
