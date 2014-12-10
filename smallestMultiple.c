7d3d083fe9413c97902f50385fedac260b305e35
Hurkpaburk <Hurkpaburk@users.noreply.github.com>
2014-12-10 15:58:05 +0100

    Update smallestMultiple.c

--- smallestMultiple.c
+++ smallestMultiple.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    
-    int result = smallestMultiple(10);
-    printf("Result of problem 5: %d\n",result);
-    
-}
-
 int smallestMultiple(int num) {
     
     int i;
     int number = num;
     int test = 1;
     while (test != 0) {
-        test = 0;    
-        for (i=1;i<=num;i++) {
-                //if (number%i) {
-                    //printf("%d\n",number%i);
-                    test = test + number%i;
-                //}
-                
-         }
+        test = 0;
         number = number +1;
-        printf("%d\n",number);
-
+        for (i=1;i<=num;i++) {
+                    test += number%i;
+         }
     }
-    //printf("%d\n",test);
     return number;
 }
