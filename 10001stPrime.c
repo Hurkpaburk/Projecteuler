cdb7006f9abcb7059fbb1dd5054a29f03fa61160
Hurkpaburk <Hurkpaburk@users.noreply.github.com>
2014-12-10 16:20:22 +0100

    Create 10001stPrime.c

---
+++ 10001stPrime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    int result = 10001stPrime(10001);
+    printf("Result of problem 7: %d\n",result);
+    
+}
+
+int 10001stPrime(int num) {
+    
+    int i = 1;
+    int j = 3;
+    int primes[num];
+    primes[0] = 2;
+    
+    while (i != num) {
+        if (j/)
+        
+        j = j+2;
+    }
+        
+ return num;
+}
