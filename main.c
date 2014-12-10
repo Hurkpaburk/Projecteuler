b2a278610a2896e16b6e9fb8f3760f535470280c
Hurkpaburk <Hurkpaburk@users.noreply.github.com>
2014-12-10 16:04:08 +0100

    Update main.c
    Changed first letter to Capital Letter

--- main.c
+++ main.c
@@ -4,18 +4,18 @@ int main() {
     
     int result;
     
-    result = multiplesOfThreeAndFive(1000);
+    result = MultiplesOfThreeAndFive(1000);
     printf("Result of problem 1: %d\n",result);
     
-    result = evenFibonacciNumbers(4000000);
+    result = EvenFibonacciNumbers(4000000);
     printf("Result of problem 2: %d\n",result);
     
-    result = largestPrimeFactor(600851475143);
+    result = LargestPrimeFactor(600851475143);
     printf("Result of problem 3: %d\n",result);
     
-    result = largestPalindromeProduct(3);
+    result = LargestPalindromeProduct(3);
     printf("Result of problem 4: %d\n",result);
     
-    int result = smallestMultiple(20);
+    int result = SmallestMultiple(20);
     printf("Result of problem 5: %d\n",result);
 }
